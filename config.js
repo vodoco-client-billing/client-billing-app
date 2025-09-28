14c4bc3f3998f625a1fb43299232ed8126b7e85c
vodoco-client-billing <hello.vodoco@outlook.com>
2025-09-29 09:41:02 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,17 @@
 // config.js
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Identificador de la app (queda como carpeta en Firestore)
+window.__app_id = "clientbillinglive";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Configuración de Firebase
+window.__firebase_config = JSON.stringify({
   apiKey: "AIzaSyDEiUakvzE6acvLMwSVYoqqP7WKKHhyR4U",
   authDomain: "clientbillinglive.firebaseapp.com",
   projectId: "clientbillinglive",
   storageBucket: "clientbillinglive.firebasestorage.app",
   messagingSenderId: "400586974621",
   appId: "1:400586974621:web:1acecdaa4158ab0de4bb74"
-};
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-// Por ahora lo dejamos en null, más adelante podemos poner un token fijo
+// De momento usamos null, así entra con login anónimo
 window.__initial_auth_token = null;
