109603c7d60bdfdcc814a5d73710126e90c01a72
vodoco-client-billing <hello.vodoco@outlook.com>
2025-09-29 14:23:45 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,6 @@
 // config.js
-
-// Identificador de la app (queda como carpeta en Firestore)
 window.__app_id = "clientbillinglive";
 
-// Configuración de Firebase
 window.__firebase_config = JSON.stringify({
   apiKey: "AIzaSyDEiUakvzE6acvLMwSVYoqqP7WKKHhyR4U",
   authDomain: "clientbillinglive.firebaseapp.com",
@@ -13,5 +10,5 @@ window.__firebase_config = JSON.stringify({
   appId: "1:400586974621:web:1acecdaa4158ab0de4bb74"
 });
 
-// De momento usamos null, así entra con login anónimo
-window.__initial_auth_token = null;
+// 👉 pega aquí el token que te imprimió `node mint.js`
+window.__initial_auth_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9....";
